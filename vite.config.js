a5675fa54a23102f727cb3cfbd7f55a37d9a71d1
bidblues <bidgov4@gmail.com>
2024-09-05 12:21:52 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0' // Listen on all network interfaces
   },
-  plugins: [react()]
-  base: '/<portfolio-react>/',
+  plugins: [react()],
+  base: '/portfolio-react/',
 });
